989b459afe40ecb4bb9737edeefb1ef9a5b8dde0
Tristan Lee Mitchell <tristanmitchell2113@gmail.com>
2022-05-18 18:27:28 +1000

    Update

--- CovidProject/program.cpp
+++ CovidProject/program.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 void menu();
-void studentin();
-void studentout();
-void staffin();
-void staffout();
+void yourdetails();
+void teststatus();
+void displaylocations();
+void updatepatient();
+void displaypatient();
 
 
 int main() {
@@ -16,34 +17,129 @@ int main() {
 	return 0;
 }
 
-void menu() {
+void menu() {	//Main Menu
 	int choice;
 	do {
 		cout << "------------------------------------------------------------" << endl;
 		cout << "Press number 1 - Enter your detail for COVID-Test Recommendation" << endl;
-		cout << "Press number 2 - Submit your COVID " << endl;
-		cout << "Press number 3 - Display student record" << endl;
-		cout << "Press number 4 - Display staff record" << endl;
+		cout << "Press number 2 - Submit your COVID Test Status & Update the Location Database" << endl;
+		cout << "Press number 3 - Display the Updated Location (High Risk for COVID)" << endl;
+		cout << "Press number 4 - Update COVID Patient Details" << endl;
+		cout << "Press number 5 - Display the COVID Positive Patient Details" << endl;
+		cout << "Press number 6 - Quit" << endl;
 		cin >> choice;
 		cout << "------------------------------------------------------------" << endl;
-	} while (choice < 1 || choice > 4);
+	} while (choice < 1 || choice > 6);
 	switch (choice) {
 	case 1:
-		studentout();
+		yourdetails();
 		break;
 	case 2:
-		staffout();
+		teststatus();
 		break;
 	case 3:
-		studentin();
+		displaylocations();
 		break;
 	case 4:
-		staffin();
+		updatepatient();
+		break;
+	case 5:
+		displaypatient();
+		break;
+	case 6:
 		break;
 	}
 }
 
-void studentout() {
+void yourdetails() {
+	cout << "Enter your Details" << endl;
+	string name;
+	string id;
+	string course;
+	string units;
+	string classtime;
+	cout << "Enter ID: ";
+	cin >> id;
+	cout << "Enter name: ";
+	cin >> name;
+	cout << "Enter course: ";
+	cin >> course;
+	cout << "Enter Units: ";
+	cin >> units;
+	cout << "Enter Class time: ";
+	cin >> classtime;
+
+	ofstream stafffile;
+	stafffile.open("database.txt", ofstream::app);
+	stafffile << name << "\t" << id << "\t" << course << "\t" << units << "\t" << classtime << endl;
+	stafffile.close();
+}
+
+void teststatus() {
+
+}
+
+void displaylocations() {	//Display Locations
+	string line;
+	cout << "High Risk Locations:" << endl;
+	ifstream locations("locations.txt");
+	if (locations.is_open())
+	{
+		while (getline(locations, line))
+		{
+			cout << line << endl;
+		}
+	}
+	locations.close();
+	main();
+}
+
+void updatepatient() {
+	string id;
+	cout << "Enter Patient ID to Update: ";
+	cin >> id;
+	string line;
+	ifstream database("database.txt");
+	if (database.is_open())
+	{
+		while (getline(database, line))
+		{
+			if (line.substr(0, 3) == id) {	//Successful find of ID
+
+			}	
+		}
+	}
+	else cout << "Unable to open file";
+	database.close();
+
+
+}
+
+void displaypatient() {		//Display Patients
+	string line;
+	cout << left << "Id" << "\t" << "Name" << "\t\t" << "DOB" << "\t" << "Address" << "\t" << "Visited Location"  << "Date" << "\t" << "Travel" << "\t" << "Covid" << "\t" << "Status" << endl;
+
+	ifstream database("database.txt");
+	if (database.is_open())
+	{
+		if (database.peek() == ifstream::traits_type::eof()) {
+			cout << "Database file is empty!" << endl;
+			main();
+		}
+		while (getline(database, line))
+		{
+			cout << line << endl;
+		}
+	}
+	else cout << "Unable to open file";
+	database.close();
+	main();
+}
+
+
+
+/*
+void yourdetails() {
 	string name;
 	string id;
 	string course;
@@ -114,3 +210,4 @@ void staffout() {
 
 	main();
 }
+*/
